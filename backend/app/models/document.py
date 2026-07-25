74efc554cff7b5cccd745ce8a37c3591928cd238
kaur-devs <kaur.tarman006@gmail.com>
2026-07-25 23:36:16 +0530

    document table

---
+++ backend/app/models/document.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column,Integer,String,DateTime 
+from datetime import datetime
+from app.database.database import Base
+
+class Document(Base):
+    __tablename__ = "documents"
+
+    id = Column(Integer,primary_key=True,index=True)
+    filename = Column(String,nullable=False)
+    filepath = Column(String,nullable=False)
+    status = Column(String,default="uploaded")
+    created_at = Column(DateTime,default=datetime.utcnow)
--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI
 from app.api.routes.root import router as root_router
 from app.api.routes.health import router as health_router 
+from app.database.database import Base, engine
+from app.models.document import Document 
 
 app = FastAPI(
     title = "RAG",
     version = "1.0.0"
 )
+
+Base.metadata.create_all(bind=engine)
 
 app.include_router(root_router)
 app.include_router(health_router)
