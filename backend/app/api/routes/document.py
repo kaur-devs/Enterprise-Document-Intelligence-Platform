7c698b123904f851da4500eb3652c448b65fcfa8
kaur-devs <kaur.tarman006@gmail.com>
2026-07-27 22:41:47 +0530

    document creation API

--- backend/app/api/routes/document.py
+++ backend/app/api/routes/document.py
@@ -1,7 +1,18 @@
-from fastapi import APIRouter 
+from fastapi import APIRouter , Depends 
+from sqlalchemy.orm import Session
+from app.database.database import get_db 
+from app.schemas.document import DocumentCreate , DocumentResponse
+from app.services.document_service import DocumentService
 
 router = APIRouter(
     prefix = "/documents",
     tags = ["Documents"]
 )
+
+service = DocumentService()
+
+@router.post("/",response_model=DocumentResponse)
+def create_document(document:DocumentCreate,db:Session=Depends(get_db)):
+    return service.create_document(db,document)
+
 
